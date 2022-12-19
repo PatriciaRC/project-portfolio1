e2869216c526ec240ad9a91351d0022b5a02e404
PatriciaRC <patriciaromero2106@gmail.com>
2022-12-19 17:47:49 +0100

    adding footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   template: `
+   <footer class="footer">
+  <div class="content has-text-centered">
     <p>
-      footer works!
+      Made by <a href="https://www.linkedin.com/in/patricia-r-b86937242/">Patri</a> on December 2022 as a portfolio practice!
     </p>
+  </div>
+</footer>
   `,
   styles: [
   ]
