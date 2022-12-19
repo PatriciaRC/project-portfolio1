e5a7bdb27d561f43f92fd28d4bed8a880bf5c141
PatriciaRC <patriciaromero2106@gmail.com>
2022-12-19 17:57:16 +0100

    adjusting bulma and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <router-outlet></router-outlet>
+    <!-- HEADER -->
+   <app-header></app-header>
+
+   <!-- HOME -->
+
+   <app-home></app-home>
+   
+   <!-- ROUTES -->
+      <router-outlet></router-outlet>
+   
+   <!-- FOOTER -->
+      <app-footer></app-footer>
   `,
   styles: []
 })
